0f0ddd51b0043f380ce301e31be011e6d6bc116a
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-06 10:30:02 +0700

    login

--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -1,33 +1,29 @@
-describe("The Login Page", () => {
+describe("Login", () => {
   beforeEach(() => {
-    // reset and seed the database prior to every test
-    // cy.exec("npm run db:reset && npm run db:seed");
-
-    // seed a user in the DB that we can control from our tests
-    // assuming it generates a random password for us
-    cy.request("POST", "/test/seed/user", { username: "jane.lane" })
-      .its("body")
-      .as("currentUser");
+    // Menjalankan langkah persiapan sebelum setiap pengujian
+    cy.visit("http://localhost:3000/authentication/login");
+    cy.wait(2000);
+    cy.intercept("POST", "http://localhost:3030/authentication").as(
+      "loginRequest"
+    );
+    // cy.wait("@loginRequest");
   });
 
-  it("sets auth cookie when logging in via form submission", function () {
-    // destructuring assignment of the this.currentUser object
-    const { username, password } = this.currentUser;
+  it("Login gagal dengan kredensial salah", () => {
+    cy.get('input[name="email"]').type("admin@ujian.com");
+    cy.get('input[name="password"]').type("135677");
+    cy.get('button[type="submit"]').click();
 
-    cy.visit("http://localhost:3000/authentication/login");
+    // Memverifikasi bahwa pesan kesalahan muncul
+    cy.contains("Email atau password salah").should("be.visible", {
+      timeout: 5000,
+      delay: 2000,
+    });
+  });
 
-    cy.get("input[name=username]").type(username);
-
-    // {enter} causes the form to submit
-    cy.get("input[name=password]").type(`${password}{enter}`);
-
-    // we should be redirected to /dashboard
-    cy.url().should("include", "/dashboard");
-
-    // our auth cookie should be present
-    cy.getCookie("your-session-cookie").should("exist");
-
-    // UI should reflect this user being logged in
-    cy.get("h1").should("contain", "jane.lane");
+  it("Login berhasil", () => {
+    cy.get('input[name="email"]').type("admin@ujian.com");
+    cy.get('input[name="password"]').type("123");
+    cy.get('button[type="submit"]').click();
   });
 });
