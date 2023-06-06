3c75436200b502ac010f57268043e1cd60f77fb9
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-06 21:30:00 +0700

    create pelajaran

--- cypress/e2e/pelajaran/create_pelajaran.cy.js
+++ cypress/e2e/pelajaran/create_pelajaran.cy.js
@@ -4,12 +4,14 @@ describe("Data Creation", () => {
   });
 
   it("Membuat data setelah login", () => {
+    const randomValue = Math.random().toString(36).substring(7);
     cy.login();
     cy.contains("Konfigurasi").click();
     cy.contains("Pelajaran").click();
-    cy.wait(3000);
+    cy.wait(2000);
     cy.contains("Tambahkan").click();
-    cy.get('input[name="pelajaran"]').type("Sosiologi");
+    cy.wait(2000);
+    cy.get('input[name="pelajaran"]').type(randomValue);
     cy.get('button[type="submit"]').click();
     cy.contains("Berhasil menambahkan pelajaran").should("be.visible");
   });
