69c1488a350e4bd80b245410b4501813e6296946
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-06 21:11:30 +0700

    add  command login #1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,6 +11,17 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add("login", () => {
+  cy.visit("http://localhost:3000/authentication/login");
+  cy.wait(1000);
+  cy.get('input[name="email"]').type("admin@ujian.com");
+  cy.get('input[name="password"]').type("123");
+  cy.get('button[type="submit"]').click();
+  cy.contains("Hi,").should("be.visible", {
+    timeout: 3000,
+    delay: 2000,
+  });
+});
 //
 //
 // -- This is a child command --
