ae54aeebe06790ff8dca3e7d4cf7ae234e363311
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-06-06 10:49:59 +0700

    login #2

--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -1,29 +1,29 @@
 describe("Login", () => {
   beforeEach(() => {
-    // Menjalankan langkah persiapan sebelum setiap pengujian
+    cy.viewport(1536, 960);
+    // cy.viewport("iphone-5");
     cy.visit("http://localhost:3000/authentication/login");
     cy.wait(2000);
     cy.intercept("POST", "http://localhost:3030/authentication").as(
       "loginRequest"
     );
-    // cy.wait("@loginRequest");
   });
 
-  it("Login gagal dengan kredensial salah", () => {
+  it("Login gagal dengan password salah", () => {
     cy.get('input[name="email"]').type("admin@ujian.com");
     cy.get('input[name="password"]').type("135677");
     cy.get('button[type="submit"]').click();
-
-    // Memverifikasi bahwa pesan kesalahan muncul
-    cy.contains("Email atau password salah").should("be.visible", {
-      timeout: 5000,
-      delay: 2000,
-    });
+    cy.contains("Email atau password salah").should("be.visible");
+    cy.wait(2000);
   });
 
   it("Login berhasil", () => {
     cy.get('input[name="email"]').type("admin@ujian.com");
     cy.get('input[name="password"]').type("123");
     cy.get('button[type="submit"]').click();
+    cy.contains("Hi,").should("be.visible", {
+      timeout: 5000,
+      delay: 2000,
+    });
   });
 });
